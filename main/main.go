38f85134bff30d24c0bf9e45b6cc795195b60d0f
AndreyVLZ <root@workspace7a6fd5tli0yabz3c.go-cli-667949f57-8gpw8>
2020-01-16 15:02:16 +0000

    111

--- main/main.go
+++ main/main.go
@@ -15,11 +15,13 @@ import (
 )
 
 // var x string = "Hello World"
+var ClientIdjs string = "https://a-v2.sndcdn.com/assets/49-d7adc028-3.js"
 var myclientid string = "L1Tsmo5VZ0rup3p9fjY67862DyPiWGaG"
 
 func main() {
 	// main1()
-	main2()
+	// main2()
+	GetClientId()
 }
 
 func main1() {
@@ -47,7 +49,7 @@ func main2() {
 		panic(err)
 	}
 	mb4 := GetBody(myjson["url"])
-	fmt.Println(mb4)
+	// fmt.Println(mb4)
 	playlist, _ := m3u8.ReadString(mb4)
 	// fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 	// fmt.Println(playlist.Items[6])
@@ -66,6 +68,29 @@ func main2() {
 	// mb5 := GetBody("https://cf-hls-media.sndcdn.com/media/3671770/3831429/YSPVFcL40sak.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLWhscy1tZWRpYS5zbmRjZG4uY29tL21lZGlhLyovKi9ZU1BWRmNMNDBzYWsuMTI4Lm1wMyIsIkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTU3OTE3NjU5Mn19fV19&Signature=OdW-~cv-mV1UxzpEWWuj-YbX9sAvQd58ovB-LC0-Wa-K7EwhvVvOzU~JchEMrL49Yd8XMtI10boRhcucJppGNgbeFNdDS9Mq1mt7Ok-3JfQjbtbyY-xrOkwdEjQ9bTtr0zac9WhC2ILnFb97Xcen6IqDX1YjRK3VBVtoln3exMxnyexpNHYjMAmUF5bIshVSPMqL6a~a5NjEt6jK1bRsxfYIPpRKjrKz3AWnwCwRhO8U2MdCHg96YaWzSecF5OhO5k7WcU4L0DEFjJfxS3DvMxHTdc6KwpS4IYTwuc1e~4tEybT6J59HIsjQYXxHm7R-HrpDiEjwcN39kS1stHR9jA__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ")
 	// fmt.Println(mb5)
 
+}
+
+//GetClientId ...
+func GetClientId() {
+	jsString := GetBody(ClientIdjs)
+	fmt.Println()
+	f, err := os.OpenFile("testJS.txt", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	l, err := f.WriteString(jsString)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 // WriteToFile ...
